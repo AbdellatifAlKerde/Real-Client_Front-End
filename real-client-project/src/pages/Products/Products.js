eb0397558447eb8f60ace110c9e621c1b3b2d9f2
hassanadawieh <adawiehhassan@gmail.com>
2023-04-28 20:18:14 +0300

    fetching data for the peoduct

--- real-client-project/src/pages/Products/Products.js
+++ real-client-project/src/pages/Products/Products.js
@@ -1,9 +1,37 @@
-import React from 'react';
-
+import {React , useEffect , useState } from 'react';
+import axios from "axios"
+import Card from "./Card/Card"
+import "./Products.css"
 const Products = () => {
+  const [products , setProducts] = useState([])
+
+    useEffect( () => {
+      axios
+        .get(`http://localhost:6010/api/product`)
+        .then((response) => {
+          if (response.status === 200) {
+            setProducts(response.data.items)
+            console.log(response.data.items);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="product-container">
+      <div className='search'>
+        
+      </div>
+      <div className='products'>
+                {products.map((element)=>(
+                    <Card image={`${process.env.REACT_APP_IMAGESURL}${element.image}`} price={element.price}  name={element.name} description={element.description} category={element.category.name} />
+                    ))}
+      </div>
+      <div className='paggination'>
+
+      </div>
     </div>
   );
 }
