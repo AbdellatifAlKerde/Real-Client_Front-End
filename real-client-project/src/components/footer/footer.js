9631f5b7e48c98c93875c89c7a57fc7ec5edfb09
Belal Khaleel <belalkhaleel19@gmail.com>
2023-04-28 07:27:51 +0300

    changed alignment of footer and added categories

--- real-client-project/src/components/footer/footer.js
+++ real-client-project/src/components/footer/footer.js
@@ -1,52 +1,100 @@
-import "./footer.css";
-import RMZNA from "../../../src/images/logo-for-web.png";
-import { NavLink } from "react-router-dom";
-import facebookIcon from "../../../src/images/facebook.png";
-import instagramIcon from "../../../src/images/instagram-logo.png";
-import whatsappIcon from "../../../src/images/whatsapp.png";
+import React from 'react';
+import './footer.css';
+import RMZNA from '../../../src/images/logo-for-web.png';
+import { NavLink } from 'react-router-dom';
+import { FaFacebookF, FaInstagram, FaWhatsapp, FaMapMarkerAlt, FaPhone, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
-  const year = new Date().getFullYear();
-
   return (
-    <footer>
+    <footer className="footer">
       <div className="footer-logo">
-        <img src={RMZNA} alt="rmnza-logo"></img>
+        <img src={RMZNA} alt="Logo" />
       </div>
+
       <div className="footer-links">
+        <h3>Navigation</h3>
+        <NavLink exact to="/" className="footer-link">
+          Home
+        </NavLink>
+        <NavLink to="/products" className="footer-link">
+          Products
+        </NavLink>
+        <NavLink to="/about-us" className="footer-link">
+          About Us
+        </NavLink>
+        <NavLink to="/contact-us" className="footer-link">
+          Contact Us
+        </NavLink>
+      </div>
+
+      <div className="footer-categories">
+        <h3>Categories</h3>
         <ul>
           <li>
-            <NavLink exact to="/" activeClassName="active-link">
-              Home
+            <NavLink to="/category1" className="footer-link">
+              Category 1
             </NavLink>
           </li>
           <li>
-            <NavLink to="/products" activeClassName="active-link">
-              Products
+            <NavLink to="/category2" className="footer-link">
+              Category 2
             </NavLink>
           </li>
           <li>
-          <a href="#about-us">About Us</a>
+            <NavLink to="/category3" className="footer-link">
+              Category 3
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact" activeClassName="active-link">
-              Contact Us
+            <NavLink to="/category4" className="footer-link">
+              Category 4
             </NavLink>
           </li>
         </ul>
       </div>
+
       <div className="footer-social-media">
-        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-          <img src={facebookIcon} alt="Facebook" />
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-          <img src={instagramIcon} alt="Instagram" />
-        </a>
-        <a href="https://www.whatsapp.com" target="_blank" rel="noreferrer">
-          <img src={whatsappIcon} alt="WhatsApp" />
-        </a>
+        <h3>Follow Us</h3>
+        <ul>
+          <li>
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+              <FaFacebookF />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+              <FaInstagram />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.whatsapp.com" target="_blank" rel="noreferrer">
+              <FaWhatsapp />
+            </a>
+          </li>
+        </ul>
       </div>
-      <div>{`Copyright © RMZNA ${year}`}</div>
+
+      <div className="footer-contact">
+        <h3>Contact Us</h3>
+        <ul>
+          <li>
+            <FaMapMarkerAlt />
+            <span>Address</span>
+          </li>
+          <li>
+            <FaEnvelope />
+            <span>Email</span>
+          </li>
+          <li>
+            <FaPhone />
+            <span>Phone</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer-copy">
+        <p>&copy; {new Date().getFullYear()} Your Website Name. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
